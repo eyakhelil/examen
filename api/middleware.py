f5bc7f6a90e67c4591cbe7cea399962d71ba3325
Eyakhelil <eyakhelil485@gmail.com>
2025-05-26 23:47:59 +0100

    update

--- api/middleware.py
+++ api/middleware.py
@@ -7,9 +7,10 @@ class ApiKeyMiddleware:
         self.api_key = getattr(settings, "API_KEY", None)
 
     def __call__(self, request):
-        if request.path == "/graphql/":
+        
+        if request.path.startswith("/graphql"):
             key = request.headers.get("X-API-KEY")
             if key != self.api_key:
                 return JsonResponse({"error": "Unauthorized"}, status=401)
-
+            
         return self.get_response(request)
--- myproject/settings.py
+++ myproject/settings.py
@@ -32,6 +32,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,19 +41,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
-    'corsheaders',
     'graphene_django',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'api.middleware.ApiKeyMiddleware'
 ]
 
